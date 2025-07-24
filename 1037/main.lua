ce3955576a007926aaafa9e96052b309901368dd
kimpure <gimleni593@gmail.com>
2025-07-24 18:04:41 +0000

    1037

--- 1037/main.lua
+++ 1037/main.lua
@@ -1,9 +1,7 @@
 --!nocheck
 r=io.read
-n,x,i,v=r"*n"
-for _=1, n do
+for _=1,r"*n" do
     v=r"*n"
-    x=(x and x>v)and x or v
-    i=(i and i<v)and i or v 
+    x,i=(x and x>v)and x or v,(i and i<v)and i or v 
 end
 print(x*i)
